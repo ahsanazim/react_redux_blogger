4ec689a6d1b40803a67882144baa0f8f7c2b1479
AhsanAzim <ahsan-azim@live.com>
2016-08-08 13:34:59 -0400

    fixed remote back to normal

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,8 +10,8 @@ export const ActionTypes = {
 };
 
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
-// const ROOT_URL = 'https://bloggredux.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://bloggredux.herokuapp.com/api';
 // const API_KEY = '?key=a_azim';
 
 export function fetchPosts() {
