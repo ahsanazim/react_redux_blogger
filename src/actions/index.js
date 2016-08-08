4444d93ce8857cb2b7d028b01423375c088391a0
AhsanAzim <ahsan-azim@live.com>
2016-08-08 13:25:24 -0400

    fixing blank update

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,8 +10,8 @@ export const ActionTypes = {
 };
 
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
-// const ROOT_URL = 'http://localhost:9090/api';
-const ROOT_URL = 'https://bloggredux.herokuapp.com/api';
+const ROOT_URL = 'http://localhost:9090/api';
+// const ROOT_URL = 'https://bloggredux.herokuapp.com/api';
 // const API_KEY = '?key=a_azim';
 
 export function fetchPosts() {
@@ -32,6 +32,7 @@ export function fetchPost(id) {
     // axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`)
     axios.get(`${ROOT_URL}/posts/${id}`)
     .then(response => {
+      console.log(response.data);
       dispatch({ type: 'FETCH_POST', payload: { post: response.data } });
     })
     .catch(error => {
